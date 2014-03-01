30e393e6c94c69ed8c3ba1559bc5896fc2561888
Miguel A. Chateloin <miguel@chateloin.com>
2014-03-01 04:02:37 -0500

    Update Assignment4.java
    Main method implemented. Tried to figure out how to generate permutations and store them in a list without an instance variable. Currently broken.

--- Assignment4.java
+++ Assignment4.java
@@ -1,48 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package cop3530;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
+/**
+ *
+ * @author Miranda
+ */
 public class Assignment4 {
 
-    public static int getMaxDistance(String word, String[] edges){
-        Map<String,Integer> charIndexes = new HashMap();
-        for(int i = 0; i<word.length(); i++)
-            charIndexes.put(word.substring(i, i+1), i);
+    public static int getMaxDistance(Map<String,Integer> word, List<String[]> edges){
         int max = 0;
-        for(String e : edges){
-            int vertexIndex1 = charIndexes.get(e.substring(0, 1));
-            int vertexIndex2 = charIndexes.get(e.substring(1));
-            int distance = Math.abs(vertexIndex1 - vertexIndex2) + 1;
-            if(distance>max) max = distance;
+        for(String[] e : edges){
+            int distance = Math.abs(word.get(e[0]) - word.get(e[1])) + 1;
+            if(distance > max) max = distance;
         }
         return max;
     }
 
-    public static void main(String args[]){
-        
-    }
 
-}
-
-private ArrayList<String> letters = new ArrayList<>();
-
-    public ArrayList<String> permutationGenerator(String Letters) 
+    public ArrayList<String> permutationGenerator(String letters)
     {
-        
-        permutation("", Letters);
-        return letters;
+        List<String> permutations = new LinkedList();
+        return permutation("", letters, permutations);
     }
 
-    
-    private void permutation(String prefix, String str) 
+
+    private void permutation(String prefix, String str, List<String> permutations)
     {
         int n = str.length();
-        if (n == 0) 
-            letters.add(prefix);
-        else 
+        if (n == 0)
+            permutations.add(prefix);
+        
+        else
         {
             for (int i = 0; i < n; i++)
            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
         }
     }
+
+    public static void main(String args[]) throws FileNotFoundException{
+        Scanner input = new Scanner(new File(""));
+        int numCases = input.nextInt();
+        for(int i=0; i<numCases; i++){
+
+            int numEdges = input.nextInt();
+            int charIndex = 0;
+            Map<String, Integer> word = new HashMap();
+            List<String[]> edges = new LinkedList();
+
+            for(int e=0; e<numEdges; e++){
+                String[] edge = input.nextLine().split(" ");
+                for(String vertex : edge){
+                    edges.add(edge);
+                    if(!word.containsKey(vertex)){
+                        word.put(vertex, charIndex++);
+                    }
+                }
+            }
+
+            List<String> permutations;
+            int minMaxDistance = word.size() - 2;
+            String bestWord = "";
+            for(String p : permutations){
+                int maxDistance = getMaxDistance(word, edges);
+                if(maxDistance < minMaxDistance){
+                    minMaxDistance = maxDistance;
+                    bestWord = p;
+                }
+                if(maxDistance == 0) break;
+            }
+            System.out.println(bestWord + " = " + minMaxDistance);
+        }
+    }
+
+}
